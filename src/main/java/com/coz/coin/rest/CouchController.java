bbfd18472d362421a68fec2c5b4a23d8293153d9
cemilcagri.cetinkaya <cemilcagri.cetinkaya@akgunyazilim.com.tr>
2018-03-01 17:49:03 +0300

    cc

--- src/main/java/com/coz/coin/rest/CouchController.java
+++ src/main/java/com/coz/coin/rest/CouchController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -17,7 +18,10 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+
 import com.coz.coin.VTUtil;
 import com.coz.coin.Constants;
 
@@ -75,28 +79,58 @@ public class CouchController  {
 
 
     @RequestMapping("/getData")
-    public void getData(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        JSONObject obj =new JSONObject();
-        //String strResult = "{\"date\":{\"offset\":{\"totalSeconds\":0,\"id\":\"Z\",\"rules\":{\"fixedOffset\":true,\"transitionRules\":[],\"transitions\":[]}},\"zone\":{\"totalSeconds\":0,\"id\":\"Z\",\"rules\":{\"fixedOffset\":true,\"transitionRules\":[],\"transitions\":[]}},\"dayOfMonth\":26,\"dayOfWeek\":\"MONDAY\",\"dayOfYear\":57,\"month\":\"FEBRUARY\",\"year\":2018,\"hour\":0,\"minute\":0,\"nano\":0,\"second\":0,\"monthValue\":2,\"chronology\":{\"id\":\"ISO\",\"calendarType\":\"iso8601\"}},\"high\":10350.11042487,\"low\":9358.52927553,\"open\":9560.19775733,\"close\":10309.58721908,\"volume\":25747541.564163,\"quoteVolume\":2614.61774974,\"weightedAverage\":9847.53567389}";
-        String strResult = "{" +
-                "\"_id\":\"448eb14a89ed2aad56627df30b00e3c4\"," +
-                "\"_rev\":\"3-0dbd37b1089f25f38fa04075bb113fa9\"," +
-                "\"Coin_Name\":\"Etherium\"," +
-                "\"Coin_Code\":\"ETH\"," +
-                "\"Type\":\"Coins\"," +
-                "\"_attachments\":{" +
-                "\"eth (1).png\":{" +
-                "\"content_type\":\"image/png\"," +
-                "\"revpos\":2," +
-                "\"digest\":\"md5-4e9JyrAqGvTDOdAuY+MAkw==\"," +
-                "\"length\":493," +
-                "\"stub\":true}}" +
-                "}";
+    public String getData(HttpServletRequest request, HttpServletResponse response) {
 
-        JSONObject obb=new JSONObject();
+        String url = Constants.databaseUrl + "/_design/" + request.getParameter("view") + "/_view/";
 
-        obj.put("data",strResult);
-        obj.put("success",true);
-        response.getWriter().write(obj.toString());
+        url += request.getParameter("view") + "?";
+        Map<String, String[]> parameterMap = request.getParameterMap();
+        for (Map.Entry<String, String[]> entry : parameterMap.entrySet()) {
+            String key = entry.getKey();
+            String[] value = entry.getValue();
+
+            if (key.equals("view")) {
+                continue;
+            }
+
+            switch (key) {
+                case "id":
+                    url = Constants.databaseUrl + "/" + value[0] + "&";
+                    break;
+                case "keys":
+                case "startkey":
+                case "endkey":
+                    url += (key + "=[");
+                    for (String val : value) {
+                        String[] subKeys = val.split(",");
+                        if (subKeys.length > 1) {
+                            url += ("[");
+                            for (String sub : subKeys) {
+                                url += ("\"" + sub + "\",");
+                            }
+                            url = url.substring(0, url.length() - 1);
+                            url += "]&";
+                        } else {
+                            url += ("\"" + val + "\",");
+                        }
+                    }
+                    url = url.substring(0, url.length() - 1);
+                    url += "]&";
+                    break;
+
+                case "key":
+                    url += (key + "=\"" + value[0] + "\"&");
+                    break;
+
+                default:
+                    url += (key + "=" + value[0] + "&");
+            }
+
+        }
+        url = url.substring(0, url.length() - 1);
+
+        ResponseEntity<String> responseEntity = restTemplate.getForEntity(url, String.class);
+        return responseEntity.getBody();
     }
+
 }
